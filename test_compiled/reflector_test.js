f5e3ec283e82f78671d6561d60567d6e09aa3356
Boughpohpue <joseph.marianson@gmail.com>
2026-02-16 03:23:36 +0100

    Test refactor

--- test_compiled/reflector_test.js
+++ test_compiled/reflector_test.js
@@ -5,40 +5,43 @@ import { ExampleClass } from './reflector_test_data.js'
 console.log("\n==================== CLASS DEFINITION ====================\n");
 console.log(ExampleClass.toString());
 
-const instance = new ExampleClass("demo");
+console.log("\n==================== CLASS REFLECTION ====================\n");
+
+console.log("Reflector.getProperties(ExampleClass):");
+console.log(Reflector.getProperties(ExampleClass));
+
+console.log("\nReflector.getMethods(ExampleClass):");
+console.log(Reflector.getMethods(ExampleClass));
+
+console.log("\nReflector.getGetters(ExampleClass):");
+console.log(Reflector.getGetters(ExampleClass));
+
+console.log("\nReflector.getSetters(ExampleClass):");
+console.log(Reflector.getSetters(ExampleClass));
+
+console.log("\nReflector.reflect(ExampleClass):");
+console.log(Reflector.reflect(ExampleClass));
+
 
 console.log("\n==================== INSTANCE REFLECTION ====================\n");
 
-console.log("getProperties(instance):");
+const instance = new ExampleClass("demo");
+console.log("const instance = new ExampleClass(\"demo\");");
+
+console.log("Reflector.getProperties(instance):");
 console.log(Reflector.getProperties(instance));
 
-console.log("\ngetMethods(instance):");
+console.log("\nReflector.getMethods(instance):");
 console.log(Reflector.getMethods(instance));
 
-console.log("\ngetGetters(instance):");
+console.log("\nReflector.getGetters(instance):");
 console.log(Reflector.getGetters(instance));
 
-console.log("\ngetSetters(instance):");
+console.log("\nReflector.getSetters(instance):");
 console.log(Reflector.getSetters(instance));
 
-console.log("\nreflect(instance):");
+console.log("\nReflector.reflect(instance):");
 console.log(Reflector.reflect(instance));
 
-console.log("\n==================== CLASS REFLECTION ====================\n");
-
-console.log("getProperties(ExampleClass):");
-console.log(Reflector.getProperties(ExampleClass));
-
-console.log("\ngetMethods(ExampleClass):");
-console.log(Reflector.getMethods(ExampleClass));
-
-console.log("\ngetGetters(ExampleClass):");
-console.log(Reflector.getGetters(ExampleClass));
-
-console.log("\ngetSetters(ExampleClass):");
-console.log(Reflector.getSetters(ExampleClass));
-
-console.log("\nreflect(ExampleClass):");
-console.log(Reflector.reflect(ExampleClass));
 
 console.log("\n==================== END OF DEMO ====================\n");
